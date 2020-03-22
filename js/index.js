739874aaedbcf1aa43d55c294f4b804d593f8b4c
KC Embrey <kc@seedcode.com>
2020-03-22 15:07:07 -0600

    Combined dropzones
    Sorting results
    Added progress bar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 /*jshint esversion: 6 */
 
-const fileType = {tcx: 'tcx', csv: 'csv'};
+const fileType = {tcx: 'application/tcx+xml', csv: 'text/csv'};
+var filesProcessed = 0;
+var filesToProcess = 0;
+var tcxSummaryData = [];
+var csvSummaryData = [];
 
   function handleDragOver(evt) {
     evt.stopPropagation();
@@ -15,56 +19,20 @@ const fileType = {tcx: 'tcx', csv: 'csv'};
     evt.target.classList.remove('dragover');
   }
 
-  function handleFileSelect(evt) {
-    // evt.stopPropagation();
-    // evt.preventDefault();    
-    var files = evt.dataTransfer && evt.dataTransfer.files[0].type !== '' ? evt.dataTransfer.files : evt.target.files; // FileList object
-    var type = evt.target.id === 'tcxdropzone' ? fileType.tcx : fileType.csv;
-
-    if (files.length <= 0){ return true;}
-
-    var summaryContainer = (type === fileType.tcx) ? document.getElementById('tcxSummaryContainer') : document.getElementById('csvSummaryContainer');
-    while (summaryContainer.lastChild){
-        summaryContainer.removeChild(summaryContainer.lastChild);
-    }
-    var dataContainer = document.createElement('table');
-    summaryContainer.appendChild(dataContainer);
-    var currentRow = document.createElement('tr');
-    currentRow.classList.add('summaryHeader');
-    if (type === fileType.tcx){
-        appendCell('Timestamp', currentRow);
-        appendCell('Activity', currentRow);
-        appendCell('Distance (km)', currentRow);
-        appendCell('Total Time', currentRow);
-        appendCell('Calories', currentRow);
-        appendCell('Avg Heartrate', currentRow);
-        appendCell('Min Heartrate', currentRow);
-        appendCell('Max Heartrate', currentRow);
-    }
-    else{
-        appendCell('Date', currentRow);
-        appendCell('Status', currentRow);
-        appendCell('Steps', currentRow);
-        appendCell('Move Minutes', currentRow);
-        appendCell('Active', currentRow);
-        appendCell('Running', currentRow);
-        appendCell('Walking', currentRow);
-        appendCell('Calories', currentRow);
-        appendCell('Avg Heartrate', currentRow);
-        appendCell('Min Heartrate', currentRow);
-        appendCell('Max Heartrate', currentRow);
-    }
-
-    dataContainer.appendChild(currentRow);
+  function handleFileSelect(evt) {   
+    var files = evt.dataTransfer && evt.dataTransfer.files[0].type !== '' ? evt.dataTransfer.files : evt.target.files;
 
 
-    // Loop through the FileList and render image files as thumbnails.
+    evt.target.classList.remove('dragover');
+    
+    filesProcessed = 0;
+    filesToProcess = files.length;
+    if (filesToProcess <= 0){ return true;}
+
+    document.documentElement.style.setProperty('--progressBarDisplay', 'block');
+
+    // Loop through the FileList
     for (var i = 0, f; f = files[i]; i++) {
-
-      // Only process image files.
-    //   if (!f.type.match('image.*')) {
-    //     continue;
-    //   }
 
       var reader = new FileReader();
 
@@ -72,163 +40,213 @@ const fileType = {tcx: 'tcx', csv: 'csv'};
       reader.onload = (function(theFile) {
         return function(e) {
 
-            var summary = (type === fileType.tcx) ? getActivitySummary(e.target.result, dataContainer) : getAggregationSummary(e.target.result, theFile.name, dataContainer);
-            
-
-            if (summary != ''){
-            //     // Add to summaries.
-            //     var span = document.createElement('div');
-            //     span.innerHTML = summary;
-                summaryContainer.appendChild(dataContainer);
-            }
+            let summaryResult = (theFile.type === fileType.tcx) ? getActivitySummary(e.target.result, tcxSummaryData) : getAggregationSummary(e.target.result, theFile.name, csvSummaryData);
+            handleFileProcessed();
         };
       })(f);
 
       // Read in the image file as a data URL.
       reader.readAsText(f);
     }
+
+    
   }
 
-  // Setup the dnd listeners.
-  var tcxdropzone = document.getElementById('tcxdropzone');
-  tcxdropzone.addEventListener('dragover', handleDragOver, false);
-  tcxdropzone.addEventListener('dragleave', handleDragLeave, false);
-  tcxdropzone.addEventListener('drop', handleFileSelect, false);
-  tcxdropzone.addEventListener('change', handleFileSelect, false);
-  var csvdropzone = document.getElementById('csvdropzone');
-  csvdropzone.addEventListener('dragover', handleDragOver, false);
-  csvdropzone.addEventListener('dragleave', handleDragLeave, false);
-  csvdropzone.addEventListener('drop', handleFileSelect, false);
-  csvdropzone.addEventListener('change', handleFileSelect, false);
+  function handleFileProcessed(){
+    filesProcessed++;
+    var percentComplete = filesProcessed / filesToProcess;
+    document.documentElement.style.setProperty('--gradientStart', (percentComplete > 50 ? (percentComplete - 50) * 200 : 0) + '%');
+    document.documentElement.style.setProperty('--gradientEnd', (percentComplete * 100) + '%');
+    if(filesProcessed === filesToProcess){
+        processSummaries();
+    }
+  }
+
+  function processSummaries(){
+    //Update Activity Reports
+    var summaryContainer = document.getElementById('tcxSummaryContainer');
+    while (summaryContainer.lastChild){
+        summaryContainer.removeChild(summaryContainer.lastChild);
+    }
+    var dataContainer = document.createElement('table');
+    summaryContainer.appendChild(dataContainer);
+    var currentRow = document.createElement('tr');
+    currentRow.classList.add('summaryHeader');
+    appendCell('Timestamp', currentRow);
+    appendCell('Activity', currentRow);
+    appendCell('Distance (km)', currentRow);
+    appendCell('Total Time', currentRow);
+    appendCell('Calories', currentRow);
+    appendCell('Avg Heartrate', currentRow);
+    appendCell('Min Heartrate', currentRow);
+    appendCell('Max Heartrate', currentRow);
+    dataContainer.appendChild(currentRow);
+
+    tcxSummaryData.sort(compareDates);
+
+    tcxSummaryData.forEach(function(summary){
+        currentRow = document.createElement('tr');
+        appendCell(summary.timestamp.format('MMM D, YYYY - HH:MM'), currentRow);
+        // appendCell(intensity + ' ' + activityType, currentRow);
+        appendCell(summary.activityType, currentRow);
+        appendCell(roundToPrecision(summary.distance / 1000, 2), currentRow);
+        appendCell(formatDuration(summary.totalTime * 1000), currentRow);
+        appendCell(roundToPrecision(summary.calories, 0), currentRow);
+        appendCell(roundToPrecision(summary.averageHR, 0), currentRow);
+        appendCell(roundToPrecision(summary.minimumHR, 0), currentRow);
+        appendCell(roundToPrecision(summary.maximumHR, 0), currentRow);
+        dataContainer.appendChild(currentRow);
+    });
+
+
+
+    //Update Daily Aggregations
+    summaryContainer = document.getElementById('csvSummaryContainer');
+    while (summaryContainer.lastChild){
+        summaryContainer.removeChild(summaryContainer.lastChild);
+    }
+    dataContainer = document.createElement('table');
+    summaryContainer.appendChild(dataContainer);
+    currentRow = document.createElement('tr');
+    currentRow.classList.add('summaryHeader');
+    summaryContainer = document.getElementById('csvSummaryContainer');
+    appendCell('Date', currentRow);
+    appendCell('Status', currentRow);
+    appendCell('Steps', currentRow);
+    appendCell('Move Minutes', currentRow);
+    appendCell('Active', currentRow);
+    appendCell('Running', currentRow);
+    appendCell('Walking', currentRow);
+    appendCell('Calories', currentRow);
+    appendCell('Avg Heartrate', currentRow);
+    appendCell('Min Heartrate', currentRow);
+    appendCell('Max Heartrate', currentRow);
+    dataContainer.appendChild(currentRow);
+
+    csvSummaryData.sort(compareDates);
+
+    csvSummaryData.forEach(function(summary){
+        currentRow = document.createElement('tr');
+        appendCell(summary.timestamp.format('MMM D, YYYY'), currentRow);
+        appendCell(summary.incompleteData ? 'INCOMPLETE' : 'complete', currentRow);
+        appendCell(summary.stepCount, currentRow);
+        appendCell(summary.moveMinutes, currentRow);
+        appendCell(formatDuration(summary.activeTime), currentRow);
+        appendCell(formatDuration(summary.timeRunning), currentRow);
+        appendCell(formatDuration(summary.timeWalking), currentRow);
+        appendCell(roundToPrecision(summary.calories, 0), currentRow);
+        appendCell(roundToPrecision(summary.averageHR, 0), currentRow);
+        appendCell(roundToPrecision(summary.minimumHR, 0), currentRow);
+        appendCell(roundToPrecision(summary.maximumHR, 0), currentRow);
+        dataContainer.appendChild(currentRow);
+    });
+  }
+
+    //Hide progress bar for now
+    document.documentElement.style.setProperty('--progressBarDisplay', 'none');
+    // Setup the dropzone listeners.
+    var tcxdropzone = document.getElementById('tcxdropzone');
+    tcxdropzone.addEventListener('dragover', handleDragOver, false);
+    tcxdropzone.addEventListener('dragleave', handleDragLeave, false);
+    tcxdropzone.addEventListener('drop', handleFileSelect, false);
+    tcxdropzone.addEventListener('change', handleFileSelect, false);
 
   
-  function getActivitySummary(data, dataContainer){
+  function getActivitySummary(data, tcxSummaryData){
     var dataObj = X2J.parseXml(data)[0];
     var activities = dataObj.TrainingCenterDatabase[0].Activities[0].Activity;
-    var activityType;
-    var distance;
-    var totalTime;
-    var calories;
-    var totalAverageReadings;
-    var averageHR;
-    var maximumHR;
-    var minimumHR;
-    var intensity;
-    var totalLaps;
-    var timestamp;
-    var summaries = '';
     for (let activity of activities){
-        activityType = activity.Notes[0].jValue;
-        totalAverageReadings = 0;
-        averageHR = 0;
-        maximumHR = 0;
-        minimumHR = 1000;
-        totalLaps = 0;
-        totalTime = 0;
-        calories = 0;
-        distance = 0;
+        let currentActivity = {
+            activityType: activity.Notes[0].jValue,
+            averageHR: 0,
+            maximumHR: 0,
+            minimumHR: 1000,
+            totalTime: 0,
+            calories: 0,
+            distance: 0,
+        };
+        let totalAverageReadings = 0;
         for (let lap of activity.Lap)
         {
-            totalLaps++;
-            timestamp = lap.jAttr.StartTime;
-            timestamp = timestamp.substring(0, timestamp.length - 8).replace('T', ' ');
+            currentActivity.timestamp = moment(lap.jAttr.StartTime);
             for (let trackpoint of lap.Track[0].Trackpoint){
                 if(typeof trackpoint.HeartRateBpm != 'undefined' && typeof trackpoint.HeartRateBpm[0].Value != 'undefined'){
-                    minimumHR = Math.min(minimumHR, parseFloat(trackpoint.HeartRateBpm[0].Value[0].jValue));
+                    currentActivity.minimumHR = Math.min(currentActivity.minimumHR, parseFloat(trackpoint.HeartRateBpm[0].Value[0].jValue));
                 }
             }
-            distance = lap.DistanceMeters ? Math.max(distance, parseFloat(lap.DistanceMeters[0].jValue)) : 0;
-            totalTime += parseFloat(lap.TotalTimeSeconds[0].jValue);
-            calories += parseFloat(lap.Calories[0].jValue);
+            currentActivity.distance = lap.DistanceMeters ? Math.max(currentActivity.distance, parseFloat(lap.DistanceMeters[0].jValue)) : 0;
+            currentActivity.totalTime += parseFloat(lap.TotalTimeSeconds[0].jValue);
+            currentActivity.calories += typeof lap.Calories !== 'undefined' ? parseFloat(lap.Calories[0].jValue) : 0;
             totalAverageReadings++;
             if(typeof lap.AverageHeartRateBpm != 'undefined' && typeof lap.AverageHeartRateBpm[0].Value != 'undefined'){
-                averageHR = (averageHR + parseFloat(lap.AverageHeartRateBpm[0].Value[0].jValue));
+                currentActivity.averageHR = (currentActivity.averageHR + parseFloat(lap.AverageHeartRateBpm[0].Value[0].jValue));
             }
             if(typeof lap.MaximumHeartRateBpm != 'undefined' && typeof lap.MaximumHeartRateBpm[0].Value != 'undefined'){
-                maximumHR = Math.max(maximumHR, parseFloat(lap.MaximumHeartRateBpm[0].Value[0].jValue));
+                currentActivity.maximumHR = Math.max(currentActivity.maximumHR, parseFloat(lap.MaximumHeartRateBpm[0].Value[0].jValue));
             }
 
             //Todo: account for multiple intensities
-            intensity = lap.Intensity[0].jValue;
+            currentActivity.intensity = lap.Intensity[0].jValue;
         }
 
-        averageHR = averageHR / totalAverageReadings;
+        currentActivity.averageHR = currentActivity.averageHR / totalAverageReadings;
 
-        if(averageHR > 0){
-            let currentRow = document.createElement('tr');
-            
-            appendCell(moment(timestamp).format('MMM D, YYYY - HH:MM'), currentRow);
-            // appendCell(intensity + ' ' + activityType, currentRow);
-            appendCell(activityType, currentRow);
-            appendCell(roundToPrecision(distance / 1000, 2), currentRow);
-            appendCell(formatDuration(totalTime * 1000), currentRow);
-            appendCell(roundToPrecision(calories, 0), currentRow);
-            appendCell(roundToPrecision(averageHR, 0), currentRow);
-            appendCell(roundToPrecision(minimumHR, 0), currentRow);
-            appendCell(roundToPrecision(maximumHR, 0), currentRow);
-            dataContainer.appendChild(currentRow);
+        if(currentActivity.averageHR > 0){
+            tcxSummaryData.push(currentActivity);
         }
+        return tcxSummaryData;
     }
 
-    return true;
+    return false;
 }
 
-  function getAggregationSummary(data, filename, dataContainer){
+  function getAggregationSummary(data, filename, csvSummaryData){
     var dataObj = csvJSON(data);
-    var distance = 0;
-    var activeTime = 0;
-    var calories = 0;
+    var result = {
+        timestamp: moment(filename.substring(0, filename.length - 4)),
+        distance: 0,
+        activeTime: 0,
+        calories: 0,
+        averageHR: 0,
+        maximumHR: 0,
+        minimumHR: 1000,
+        stepCount: 0,
+        timeRunning: 0,
+        timeWalking: 0,
+        timeInactive: 0,
+        moveMinutes: 0,
+        incompleteData: false,
+    };
     var totalAverageReadings = 0;
-    var averageHR = 0;
-    var maximumHR = 0;
-    var minimumHR = 1000;
-    var stepCount = 0;
-    var timeRunning = 0;
-    var timeWalking = 0;
-    var timeInactive = 0;
-    var moveMinutes = 0;
-    var incompleteData = false;
     for (let segment of dataObj){
         if(segment['Start time'] != '' && typeof segment['Start time'] !== 'undefined'){
-            minimumHR = segment['Min heart rate (bpm)'] != '' && typeof segment['Min heart rate (bpm)'] !== 'undefined' ? Math.min(minimumHR, extractFloatFromCSV(segment['Min heart rate (bpm)'])) : minimumHR;
-            maximumHR = segment['Max heart rate (bpm)'] != '' && typeof segment['Max heart rate (bpm)'] !== 'undefined' ? Math.max(maximumHR, extractFloatFromCSV(segment['Max heart rate (bpm)'])) : maximumHR;
+            result.minimumHR = segment['Min heart rate (bpm)'] != '' && typeof segment['Min heart rate (bpm)'] !== 'undefined' ? Math.min(result.minimumHR, extractFloatFromCSV(segment['Min heart rate (bpm)'])) : result.minimumHR;
+            result.maximumHR = segment['Max heart rate (bpm)'] != '' && typeof segment['Max heart rate (bpm)'] !== 'undefined' ? Math.max(result.maximumHR, extractFloatFromCSV(segment['Max heart rate (bpm)'])) : result.maximumHR;
             
             if (segment['Average heart rate (bpm)'] != ''){
                 totalAverageReadings++;
-                averageHR = (averageHR + extractFloatFromCSV(segment['Average heart rate (bpm)']));
+                result.averageHR = (result.averageHR + extractFloatFromCSV(segment['Average heart rate (bpm)']));
             }
-            distance += extractFloatFromCSV(segment['Distance (m)']);
-            moveMinutes += extractFloatFromCSV(segment['Move Minutes count']);
-            stepCount += extractFloatFromCSV(segment['Step count']);
-            calories += extractFloatFromCSV(segment['Calories (kcal)']);
-            timeInactive += extractFloatFromCSV(segment['Inactive duration (ms)']);
-            timeWalking += extractFloatFromCSV(segment['Walking duration (ms)']);
-            timeRunning += extractFloatFromCSV(segment['Running duration (ms)']);
-            activeTime += 900000 - extractFloatFromCSV(segment['Running duration (ms)']);
+            result.distance += extractFloatFromCSV(segment['Distance (m)']);
+            result.moveMinutes += extractFloatFromCSV(segment['Move Minutes count']);
+            result.stepCount += extractFloatFromCSV(segment['Step count']);
+            result.calories += extractFloatFromCSV(segment['Calories (kcal)']);
+            result.timeInactive += extractFloatFromCSV(segment['Inactive duration (ms)']);
+            result.timeWalking += extractFloatFromCSV(segment['Walking duration (ms)']);
+            result.timeRunning += extractFloatFromCSV(segment['Running duration (ms)']);
+            result.activeTime += 900000 - extractFloatFromCSV(segment['Running duration (ms)']);
             if (segment['Inactive duration (ms)'] == '' && segment['Walking duration (ms)'] == '' && segment['Running duration (ms)'] == ''){
-                incompleteData = true;
+                result.incompleteData = true;
             }
         }
     }
-    averageHR = averageHR / totalAverageReadings;
+    result.averageHR = result.averageHR / totalAverageReadings;
 
-    if(totalAverageReadings > 0 && averageHR > 0){
-        let currentRow = document.createElement('tr');
-        appendCell(moment(filename.substring(0, filename.length - 4)).format('MMM D, YYYY'), currentRow);
-        appendCell(incompleteData ? 'INCOMPLETE' : 'complete', currentRow);
-        appendCell(stepCount, currentRow);
-        appendCell(moveMinutes, currentRow);
-        appendCell(formatDuration(activeTime), currentRow);
-        appendCell(formatDuration(timeRunning), currentRow);
-        appendCell(formatDuration(timeWalking), currentRow);
-        appendCell(roundToPrecision(calories, 0), currentRow);
-        appendCell(roundToPrecision(averageHR, 0), currentRow);
-        appendCell(roundToPrecision(minimumHR, 0), currentRow);
-        appendCell(roundToPrecision(maximumHR, 0), currentRow);
-        dataContainer.appendChild(currentRow);
-        return true;
+    if(totalAverageReadings > 0 && result.averageHR > 0){
+        csvSummaryData.push(result);
+        return csvSummaryData;
     }
-    return false;
+    return null;
 }
 
 
@@ -253,4 +271,18 @@ function appendCell(cellValue, parent){
 
 function formatDuration(milliseconds){
     return moment.utc(moment.duration(milliseconds).as('milliseconds')).format('H[h] mm[m]');
+}
+
+function compareDates(a, b){
+    const dateA = moment(a.timestamp);
+    const dateB = moment(b.timestamp);
+
+    let comparison = 0;
+    if (dateA.isBefore(dateB)){
+        comparison = 1;
+    }
+    else if (dateB.isBefore(dateA)){
+        comparison = -1;
+    }
+    return comparison;
 }
